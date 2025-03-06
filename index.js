7fdf97f1655f7b6c6223b416859706b5ac8c97ad
Rounak Choubey <166724724+rounak333@users.noreply.github.com>
2025-03-06 12:13:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/newpage', (req, res) => {
+    res.send('This is a new page!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
